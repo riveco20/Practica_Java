e9891772849ebf3813584ddb6c9364f76a288b5c
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-02 20:49:29 -0500

    Agregando atributos y constructor en nevera

--- src/mx/com/mg/proyecto/productos/Televisor.java
+++ src/mx/com/mg/proyecto/productos/Televisor.java
@@ -1,4 +1,16 @@
 package mx.com.mg.proyecto.productos;
 
-public class Televisor {
+import mx.com.mg.proyecto.electrodomesticos.Electrodomestico;
+
+public class Televisor extends Electrodomestico {
+    public double tamaño;
+    public boolean sintonizadorTDT;
+
+    public Televisor(char comsumo, String procedencia, double tamaño, boolean sintonizadorTDT) {
+        super(comsumo, procedencia);
+        this.tamaño = tamaño;
+        this.sintonizadorTDT = sintonizadorTDT;
+    }
+
+
 }
