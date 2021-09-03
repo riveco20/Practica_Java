463e534ef8379d50c0346a59b26a19cf0f698664
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-02 20:58:09 -0500

    Actualizar Clase Nevera

--- src/mx/com/mg/proyecto/productos/Nevera.java
+++ src/mx/com/mg/proyecto/productos/Nevera.java
@@ -1,4 +1,28 @@
 package mx.com.mg.proyecto.productos;
 
-public class Nevera {
+import mx.com.mg.proyecto.electrodomesticos.Electrodomestico;
+
+import java.util.ArrayList;
+
+public class Nevera extends Electrodomestico {
+    public int capacidad;
+    public boolean sintonizadorTDT;
+    public ArrayList<Double> precios;
+
+    public Nevera(char comsumo, String procedencia, int capacidad, boolean sintonizadorTDT) {
+        super(comsumo, procedencia);
+        this.capacidad = capacidad;
+        this.sintonizadorTDT = sintonizadorTDT;
+    }
+
+    public void precioNevera() {
+        double precio = 0;
+        if (this.sintonizadorTDT == true) {
+            if (this.capacidad > 120) {
+                int capacidaDeMas = this.capacidad - 120;
+
+            }
+
+        }
+    }
 }
--- src/mx/com/mg/proyecto/productos/Televisor.java
+++ src/mx/com/mg/proyecto/productos/Televisor.java
@@ -2,15 +2,10 @@ package mx.com.mg.proyecto.productos;
 
 import mx.com.mg.proyecto.electrodomesticos.Electrodomestico;
 
-public class Televisor extends Electrodomestico {
-    public double tamaño;
-    public boolean sintonizadorTDT;
+import java.util.ArrayList;
 
-    public Televisor(char comsumo, String procedencia, double tamaño, boolean sintonizadorTDT) {
-        super(comsumo, procedencia);
-        this.tamaño = tamaño;
-        this.sintonizadorTDT = sintonizadorTDT;
+public class Televisor  {
+
     }
-
 
 }
