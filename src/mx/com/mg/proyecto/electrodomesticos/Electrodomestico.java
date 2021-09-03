e0519fed06c8afd24954cb79f1a14e1c26c06ef0
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-02 20:35:49 -0500

    Creando atributos Electrodomestico y metodos get and set

--- src/mx/com/mg/proyecto/electrodomesticos/Electrodomestico.java
+++ src/mx/com/mg/proyecto/electrodomesticos/Electrodomestico.java
@@ -1,4 +1,30 @@
 package mx.com.mg.proyecto.electrodomesticos;
 
 public class Electrodomestico {
+    protected char comsumo;
+    protected String procedencia;
+
+    public Electrodomestico(char comsumo, String procedencia){
+        this.comsumo=comsumo;
+        this.procedencia=procedencia;
+
+    }
+
+    public char getComsumo() {
+        return this.comsumo;
+    }
+
+    public void setComsumo(char comsumo) {
+        this.comsumo = comsumo;
+    }
+
+    public String getProcedencia() {
+        return this.procedencia;
+    }
+
+    public void setProcedencia(String procedencia) {
+        this.procedencia = procedencia;
+    }
+
+
 }
